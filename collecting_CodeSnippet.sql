cb564ce68ba4b62e2d0ab1a789763e17e4163632
hyunji-Hong <hyunji_hong@korea.ac.kr>
2021-09-11 15:20:30 +0900

    Update bigquery_query

---
+++ collecting_CodeSnippet.sql
@@ -0,0 +1,21 @@
+# collect considering vulnerable posts
+SELECT PostBlockVersion.PostHistoryId, PostBlockTypeId,PostBlockVersion.PostId post_id, LocalID, post_q.Id question_id, Content, Length, post_a.Score vote, post_q.Tags tags, post_history.CreationDate date
+  FROM `sotorrent-org.2020_12_31.PostBlockVersion` PostBlockVersion
+      # inner join question posts
+      INNER JOIN `sotorrent-org.2020_12_31.Posts` post_a
+      ON PostBlockVersion.PostId = post_a.Id
+      # inner join answer post 
+      INNER JOIN `sotorrent-org.2020_12_31.Posts` post_q
+      ON post_a.ParentId = post_q.Id
+      #innder join history create date
+      INNER JOIN `sotorrent-org.2020_12_31.PostHistory` post_history
+      ON PostBlockVersion.PostHistoryId = post_history.Id
+  WHERE
+    post_q.PostTypeId=1
+    AND post_a.PostTypeId=2
+    AND post_a.Score >=0
+    AND (LOWER(post_q.Tags) LIKE '%<c>%' OR LOWER(post_q.Tags) LIKE '%<c++>%'OR LOWER(post_q.Tags) LIKE '%<java>%' OR LOWER(post_q.Tags) LIKE '%<android>%')
+    AND REGEXP_CONTAINS(post_a.Body,  r'(.*)(?i)(<code>|</code>)(.*)')
+   
+  GROUP BY PostBlockVersion.PostHistoryId, PostBlockVersion.PostId, Content, post_q.Id, post_a.Score, Length, PostBlockTypeId, LocalID, post_q.Tags, post_history.CreationDate
+  ORDER BY post_a.Score DESC, PostBlockVersion.PostId DESC, PostBlockVersion.PostHistoryId DESC
---
+++ collecting_Description.sql
@@ -0,0 +1,17 @@
+  SELECT post_a.Id post_id, post_q.Title Title, post_q.Id question_id, comments.Text comment, post_a.Score vote
+  FROM `sotorrent-org.2020_12_31.Posts` post_a
+      INNER JOIN `sotorrent-org.2020_12_31.Posts` post_q
+      ON  post_q.Id = post_a.ParentId
+      INNER JOIN `sotorrent-org.2020_12_31.Comments` comments
+      ON comments.PostId = post_a.Id
+  WHERE
+    post_q.PostTypeId=1
+    AND post_a.PostTypeId = 2
+    AND post_a.Score >=0
+    AND REGEXP_CONTAINS(post_a.Body,  r'(.*)(?i)(<code>|</code>)(.*)')
+    AND (LOWER(post_q.Tags) LIKE '%<java>%' OR LOWER(post_q.Tags) LIKE '%<android>%')
+    AND (
+     REGEXP_CONTAINS(comments.Text,  r'(.*)(?i)(incorrect|vulnerab|harm|undefine|unpredict|unsafe|secur|malicious|dangerous|critical|bad|unprivileged|negative|stable|invalid|vulnerab| fault|defect|sanit|mistake|flaw| bug| hack|infinite|loop|secur|overflow|error|mistake|remote|exploit|mitigat|realloc|heap|privilege|underflow|patch|injection|segment|DoS|denial-of-service|initiali|xss|leak|authentication|authori|attack|out-of-bounds|use-after-free|trigger|dereference|corruption|crash|memory|NULL|flaw|hack| fix|change|modify|exploit|mitigat|realloc|invoke|inject|ensure|reject|initiali|leak|authori|update|attack|trigger|lock|corrupt|fail|crash|prevent|avoid|access)(.*)')
+     ) 
+  GROUP BY post_a.Id,  post_q.Title, post_q.Id, post_a.Body, comments.Text, post_a.Score
+  ORDER BY post_a.Score DESC, post_a.Id DESC
